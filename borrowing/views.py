98dd37ea472670d3c33a0b51e7d24ec3897684f1
Nazarii Khalimonov <nazar131188@gmail.com>
2025-07-12 17:23:19 +0300

    added user filter list view

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from borrowing.models import Borrowing
 from borrowing.serializers import BorrowingListSerializer, BorrowingSerializer, BorrowingDetailSerializer
@@ -8,12 +9,17 @@ class ListBorrowingsView(generics.ListAPIView):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingListSerializer
 
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).select_related("book", "user")
+
 
 class CreateBorrowingsView(generics.CreateAPIView):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class RetrieveBorrowingView(generics.RetrieveAPIView):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingDetailSerializer
+    permission_classes = (IsAuthenticated,)
